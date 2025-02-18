ef4a6a91450c425805e7233926906004e05a5823
wanghaiuestc <wanghai@uestc.edu.cn>
2025-02-18 16:30:45 +0800

    update

--- gpu-simulator/setup_environment.sh
+++ gpu-simulator/setup_environment.sh
@@ -37,7 +37,7 @@ fi
 
 if [ ! -d "$ACCELSIM_ROOT/gpgpu-sim" ] ; then
     git clone https://github.com/accel-sim/gpgpu-sim_distribution.git $ACCELSIM_ROOT/gpgpu-sim
-    git -C $ACCELSIM_ROOT/gpgpu-sim/ checkout dev
+    git -C $ACCELSIM_ROOT/gpgpu-sim/ checkout master
 fi
 
 source $ACCELSIM_ROOT/gpgpu-sim/setup_environment $ACCELSIM_CONFIG
